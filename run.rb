3ce09f26e2d8bc8913da4b88995f0b149b8f1236
Ian Sowinski <sowinj@gmail.com>
2017-02-06 17:02:04 +0100

    index.html dla działającej strony

--- run.rb
+++ run.rb
@@ -32,7 +32,7 @@ File.open('spiewnik.xml', 'w') do |f|
   f.write("</root>")
 end
 html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true )
-File.open('spiewnik.html', 'w') do |f|
+File.open('index.html', 'w') do |f|
   f.write('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Archiwum Błękitnej XIV</title><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"><link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"><style>'+style+'</style></head><body class="dark"><a class="scheme" href="#"><i class="fa fa-adjust fa-2x" aria-hidden="true"></i></a><div class="container"><div class="row"><div class="col-md-6 col-md-offset-3">')
   f.write(html.render(cont))
   f.write('</div></div></div><script src="https://code.jquery.com/jquery-3.1.1.min.js"
